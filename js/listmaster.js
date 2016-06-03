7025ef00a0186c12ff57fb5dc0019c624e9637cc
xm-71 <alexcannon2@live.com>
2016-06-03 11:39:18 -0400

    Working links and css added
    look at first js for all changes

--- js/listmaster.js
+++ js/listmaster.js
@@ -1,47 +1,125 @@
 $(document).foundation()
 
 var megaRoster = {
-  init: function() {
-    this.setupEventListeners();
-  },
+    init: function(rosterElementSelector) {
+        this.rosterElement = document.querySelector(rosterElementSelector);
+        this.setupEventListeners();
+    },
 
-  setupEventListeners: function() {
-    document.querySelector('#studentForm').onsubmit = this.addStudent.bind(this);
-  },
+    setupEventListeners: function() {
+        document.querySelector('#studentForm').onsubmit = this.addStudent.bind(this);
+    },
 
-  addStudent: function(ev) {
-    ev.preventDefault();
-    var f = ev.currentTarget;
-    var studentName = f.studentName.value;
-    var item = this.buildListItem(studentName);
-    var list = document.querySelector('#studentList');
-    list.appendChild(item);
-    f.reset();
-    f.studentName.focus();
-  },
+    addStudent: function(ev) {
+        ev.preventDefault();
+        var f = ev.currentTarget;
+        var studentName = f.studentName.value;
+        var item = this.buildListItem(studentName);
+        this.prependChild(this.rosterElement, item);
+        f.reset();
+        f.studentName.focus();
+    },
 
-  buildListItem: function(studentName) {
-    var item = document.createElement('li');
-    var deleteLink = this.buildLink({
-      text: 'remove',
-      handler: function(ev){
-        var list = item.parentElement;
-        list.removeChild(item);
-      },
-    })
-    item.innerText = studentName;
-    item.appendChild(deleteLink);
-    return item;
-  },
+    prependChild: function(parent, child) {
+        parent.insertBefore(child, parent.firstChild);
+    },
 
-  buildLink: function(options){
-    var link = document.createElement('a');
-    link.href = '#';
-    link.innerText = options.text;
-    link.onclick = options.handler;
-    return link;
+    buildListItem: function(studentName) {
+        var item = document.createElement('li');
+        var span = document.createElement('span');
+        span.innerText = studentName;
+        span.className = 'studentName';
+        item.appendChild(span);
+        this.appendLinks(item);
 
-  },
+        return item;
+    },
+
+    promote: function(item) {
+        this.prependChild(this.rosterElement, item);
+    },
+
+    moveUp: function(item) {
+        var previousElement = item.previousElementSibling;
+        this.rosterElement.insertBefore(item, previousElement);
+    },
+
+    moveDown: function(item) {
+        this.moveUp(item.nextElementSibling);
+    },
+
+    toggleEditable: function(el) {
+        var toggleElement = el.parentElement.querySelector('.toggleEdit');
+        if (el.contentEditable === "true") {
+            el.contentEditable = "false";
+            toggleElement.innerHTML = "edit";
+        } else {
+            el.contentEditable = "true";
+            toggleElement.innerHTML = "update";
+            el.focus();
+        }
+    },
+
+    appendLinks: function(item) {
+        var span = document.createElement('span');
+        span.className = 'actions';
+
+        var deleteLink = this.buildLink({
+            text: 'remove',
+            handler: function(ev) {
+                this.rosterElement.removeChild(item);
+            }
+        });
+
+        var promoteLink = this.buildLink({
+            text: 'promote',
+            handler: function() {
+                this.promote(item);
+            }
+        });
+
+        span.appendChild(deleteLink);
+        span.appendChild(promoteLink);
+
+        span.appendChild(this.buildLink({
+            text: 'up',
+            className: 'up',
+            handler: function() {
+                if (item !== this.rosterElement.firstElementChild) {
+                    this.moveUp(item);
+                }
+            }
+        }));
+
+        span.appendChild(this.buildLink({
+            text: 'down',
+            className: 'down',
+            handler: function() {
+                if (item !== this.rosterElement.lastElementChild) {
+                    this.moveDown(item);
+                }
+            }
+        }));
+
+        span.appendChild(this.buildLink({
+            text: 'edit',
+            className: 'toggleEdit',
+            handler: function() {
+                this.toggleEditable(item.querySelector('span.studentName'));
+            }
+        }));
+
+        item.appendChild(span);
+    },
+
+    buildLink: function(options) {
+        var link = document.createElement('a');
+        link.href = '#';
+        link.innerText = options.text;
+        link.onclick = options.handler.bind(this);
+        link.className = options.className;
+        return link;
+    },
 }
 
-megaRoster.init();
+megaRoster.init('#studentList');
